77c5364e26908c00e96e74db7c38c7e60f098e08
chope9894 <jeppejensen82009@gmail.com>
2025-11-21 08:38:11 +0100

    please

--- fetchDemo/src/main.jsx
+++ fetchDemo/src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router-dom'
+import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <BrowserRouter>
+  // <StrictMode> remove
+    <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
   // </StrictMode>,
 )
